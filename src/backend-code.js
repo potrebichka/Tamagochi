9cb60f5fd4831b629b2a7a86376a1bba3f691bf1
Ugonz86 <ugonzalez86@gmail.com>
2019-07-02 10:43:34 -0700

    This will commit the recent additions

--- src/backend-code.js
+++ src/backend-code.js
@@ -1,1 +1,149 @@
+import { takeAction, status } from './main.js';
 
+export class Creature {
+  constructor(){
+    this.food = 30;
+    this.play = 30;
+    this.energy = 30;
+    this.toilet = 30;
+    this.love = 30;
+    this.sick = false;
+    this.dirty = false;
+    this.sad = false;
+    this.criticalCondition = false;
+    this.heDead = false;
+    this.gone = false;
+    this.dirtyCounter = 0;
+  }
+
+  reduceStats(){
+    let interval = setInterval(() => {
+      takeAction();
+      if(this.sick === true){
+        this.food -= 2;
+        this.play -= 2;
+        this.energy -= 2;
+        this.toilet -= 2;
+        this.love -= 2;
+        if(this.criticalCondition === true){
+          this.energy += 7;
+        }
+      } else {
+        this.food--;
+        this.play--;
+        this.energy--;
+        this.toilet--;
+        this.love--;
+        if(this.criticalCondition  === true){
+          this.energy += 6;
+        }
+      }
+      if (this.food < 0) {
+        this.food = 0;
+      }
+      if (this.play < 0) {
+        this.play = 0;
+      }
+      if (this.energy < 0) {
+        this.energy = 0;
+      }
+      if (this.toilet < 0) {
+        this.toilet = 0;
+      }
+      if (this.love < 0) {
+        this.love = 0;
+      }
+      this.checkStatus();
+      if (this.dirty === true) {
+        this.dirtyCounter++;
+      }
+      if (this.dirtyCounter >= 4) {
+        this.dirty = false;
+        this.dirtyCounter = 0;
+      }
+      status();
+      if(this.heDead){
+        clearInterval(interval);
+      }
+    }, 1000);
+  }
+
+  feed(){
+    if(this.food <= 25){
+      this.food += 5;
+    } else {
+      this.food = 30;
+    }
+    this.checkStatus();
+  }
+  play(){
+    if(this.play <= 25){
+      this.play += 5;
+    } else {
+      this.play = 30;
+    }
+    this.checkStatus();
+  }
+  rest(){
+    if(this.energy <= 25){
+      this.energy += 5;
+    } else {
+      this.energy = 30;
+    }
+    this.checkStatus();
+  }
+  bathroom(){
+    if(this.toilet <= 25){
+      this.toilet += 5;
+    } else {
+      this.toilet = 30;
+    }
+    this.checkStatus();
+  }
+  love(){
+    if(this.love <= 25){
+      this.love += 5;
+    } else {
+      this.love = 30;
+    }
+    this.checkStatus();
+  }
+  clean(){
+    this.dirtyCounter += 0.5;
+    this.checkStatus();
+  }
+
+  checkStatus(){
+    if((this.food + this.play + this.energy + this.toilet + this.love) < 100){
+      this.sick = true;
+    } else {
+      this.sick = false;
+    }
+
+    if(this.toilet === 0) {
+      this.dirty = true;
+      this.toilet = 30;
+    }
+
+    if(this.play === 0) {
+      this.sad = true;
+    } else {
+      this.sad = false;
+    }
+
+    if(this.energy === 0){
+      this.criticalCondition = true;
+    } else if (this.criticalCondition === true && this.energy === 25) {
+      this.criticalCondition = false;
+    }
+
+    if(this.food === 0){
+      takeAction();
+      this.heDead = true;
+    }
+
+    if(this.love === 0){
+      this.gone = true;
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,91 @@ import './styles.css';
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Tamagotchi } from '.main.js';
+import {Creature} from './backend-code.js';
+
+let tamagotchi;
+
+function addUser() {
+  $("#startGame").click(function(){
+    tamagotchi = new Creature();
+    $("#startGame").hide();
+    $("#feed, #play, #rest, #bathroom, #love").delay(800).fadeIn()();
+    tamagotchi.reduceStats();
+  });
+
+function takeAction(){
+  if (!tamagotchi.heDead) {
+    $("#feed").html(`Food Level: ${tamagotchi.food}`);
+    $("#play").html(`Happiness Level: ${tamagotchi.play}`);
+    $("#rest").html(`Energy Level: ${tamagotchi.energy}`);
+    $("#bathroom").html(`Toilet Satisfaction: ${tamagotchi.toilet}`);
+    $("#love").html(`Creature love level: ${tamagotchi.love}`);
+    if(tamagotchi.heDead === true){
+      $("button").hide();
+    }
+  }
+}
+
+  $("#feed").click(function(){
+    tamagotchi.feed();
+    takeAction();
+  });
+  $("#play").click(function(){
+    tamagotchi.play();
+    takeAction();
+  });
+  $("#rest").click(function(){
+    tamagotchi.rest();
+    takeAction();
+  });
+  $("#bathroom").click(function(){
+    tamagotchi.bathroom();
+    takeAction();
+  });
+  $("#love").click(function(){
+    tamagotchi.love();
+    takeAction();
+  });
+}
+
+function status() {
+  if (tamagotchi.sick) {
+    $("#sick").text("I'm sick!");
+  } else {
+    $("#sick").empty();
+  }
+  if (tamagotchi.criticalCondition) {
+    $("#criticalCondition").text("I need rest and need to sleep!");
+  } else {
+    $("#criticalCondition").empty();
+  }
+  if (tamagotchi.heDead) {
+    $("#heDead").text("Tamagotchi is dead. Game over!");
+  } else {
+    $("#heDead").empty();
+  }
+  if (tamagotchi.dirty) {
+    $("#dirty").text("Woops! I pooped myself!");
+  } else {
+    $("#dirty").empty();
+  }
+  if (tamagotchi.gone) {
+    $("#gone").text("Tamagotchi felt ignored and is now gone!");
+  } else {
+    $("#gone").empty();
+  }
+  if (tamagotchi.sad) {
+    $("#sad").text("I'm unhappy and need some space. TTYL!");
+  } else {
+    $("#sad").empty();
+  }
+}
+//
+// $(function() {
+//   addUser();
+//
+//
+// });
 
 $(document).ready(function() {
   $('#newUser').submit(function(event) {
@@ -13,10 +97,6 @@ $(document).ready(function() {
 
    $("#greeting").text("Welcome " + userName1 + "!")
 
-   export class Tamagotchi {
-
-      
-   }
 
     });
   });
